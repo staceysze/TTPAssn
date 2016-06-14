12e02b4961bd2f7cae5ca748a98b13a7de96d7d6
staceysze <staceysze@gmail.com>
2016-06-14 13:06:10 -0400

    First Commit

---
+++ inspiration_app/app.js
@@ -0,0 +1,26 @@
+//order is important
+
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+app.get("/",function(req,res) {
+  res.render('home');
+});
+
+app.get("/about", function(req, res){
+  res.render('about');
+});
+
+app.get("/inspiration",function(req, res) {
+  res.render('inspiration');
+});
+
+app.listen(3000,function() {
+  console.log('Inspiration app listening on port 3000');
+});
+
+app.use(express.static('external_files'));
